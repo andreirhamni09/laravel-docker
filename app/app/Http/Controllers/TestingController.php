8bc8ca72a388a6048a6aa8de4c6c5a843a415e64
andreirhamni09 <andreirhamni09@gmail.com>
2025-05-02 19:32:17 +0700

    edit docker compose mysql tag

---
+++ app/app/Http/Controllers/TestingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
+class TestingController extends Controller
+{
+
+    public function index()
+    {
+        try {
+            $dbconnect = DB::connection()->getPDO();
+            $dbname = DB::connection()->getDatabaseName();
+            echo "Connected successfully to the database. Database name is :".$dbname;
+        } catch(Exception $e) {
+        echo "Error in connecting to the database";
+        }
+    }
+
+}
