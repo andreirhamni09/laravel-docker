08d246e962786a35fe88fff9f9299b9d8cf498f7
andreirhamni09 <andreirhamni09@gmail.com>
2025-05-02 19:33:15 +0700

    edit docker compose mysql tag

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tes-koneksi', [TestingController::class, 'index']);
 
